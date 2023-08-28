f4e0d3b3428ab42af1edb1d6f0c112f621faa903
Rizal6578 <mrizalnurrudin2018@gmail.com>
2023-08-28 20:44:19 +0700

    Menghapus semua file

--- assets/script.js
+++
@@ -1,4 +0,0 @@
-// JavaScript akan memberikan pesan saat halaman selesai dimuat
-document.addEventListener("DOMContentLoaded", function() {
-    alert("Selamat datang di halaman contoh HTML!");
-});
