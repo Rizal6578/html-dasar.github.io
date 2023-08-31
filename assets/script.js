3d164043470c03083b6046723a9e1c8ec21d00e3
Rizal6578 <mrizalnurrudin2018@gmail.com>
2023-08-31 19:54:24 +0700

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+// JavaScript akan memberikan pesan saat halaman selesai dimuat
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Selamat datang di halaman contoh HTML!");
+});
